d9147b742a716cce913e7d67b1460969ff90db65
luzaiping <luzaiping@163.com>
2017-07-11 23:39:08 +0800

    update: 函数式编程 partical 和 curry

--- functional/functional-light/chapter3/SomeNowSomeLater.js
+++ functional/functional-light/chapter3/SomeNowSomeLater.js
@@ -1,17 +1,17 @@
-function ajax(url, data, callback) {
-    console.log(url, data, callback)
-}
-
 function partical(fn, ...presetArgs) {
     return function particallyApplied(...laterArgs) {
         return fn(...presetArgs, ...laterArgs)
     }
 }
 
+function ajax(url, data, callback) {
+    console.log(url, data, callback)
+}
+
 let getPerson = partical(ajax, 'http://xxx.com/person')
 let getOrder = partical(ajax, 'http://xxx.com/order')
 let getCurrentPersonVersion1 = partical(ajax, 'http://xxx.com/person', { userId: 1111 })
-let getCurrentPersonVersion2 = partical(getPerson, { userId: 1111 })
+let getCurrentPersonVerson2 = partical(getPerson, { userId: 1111 })
 
 // version1
 let getCurrentUserVersion1 = function particallyApplied(...laterArgs) {
@@ -25,5 +25,15 @@ let getCurrentUser = function outerParticallyApplied(...outerLaterArgs) {
     }
     getPerson({ userId: 1111 }, ...outerLaterArgs)
 }
+
+function add(x, y) {
+    return x + y
+}
+
+/*[1, 2, 3, 4, 5].map(function(value) {
+    return add(3, value)
+})*/
+
+[2, 3, 3, 4, 5].map(partical(add, 3))
 
 module.exports = { partical }
---
+++ functional/functional-light/chapter3/curryHelper.js
@@ -0,0 +1,44 @@
+function curry(fn, arity = fn.length) {
+    return (function nextCurried(prevArgs) {// prevArgs 是一个数组
+        return function curried(nextArgs) {// nextArgs不是数组,只能是一个参数
+            let args = prevArgs.concat([nextArgs])
+            if (args.length >= arity) {
+                return fn(...args) // 当参数个数已经达到原始函数的parameter个数，则执行原先函数，参数是已经收集的所有arguments
+            } else {
+                return nextCurried(args) // 如果参数个数没有达到所需的个数，则返回下一个函数，接收之前已经concat的参数数组
+            }
+        }
+    })([])
+}
+
+function looseCurry(fn, arity = fn.length) {
+    return (function nextCurried(prevArgs) {// prevArgs 是一个数组
+        return function curried(...nextArgs) {// nextArgs是数组，可以是多个参数
+            let args = prevArgs.concat(nextArgs)
+            if (args.length >= arity) {
+                return fn(...args) // 当参数个数已经达到原始函数的parameter个数，则执行原先函数，参数是已经收集的所有arguments
+            } else {
+                return nextCurried(args) // 如果参数个数没有达到所需的个数，则返回下一个函数，接收之前已经concat的参数数组
+            }
+        }
+    })([])
+}
+
+function sum(...args) {
+    let count = 0
+    for ( let arg of args) {
+        count += arg
+    }
+    return count
+}
+
+let curriedSum = curry(sum, 5)
+console.log(curriedSum(1)(2)(3)(4)(5))
+console.log(curriedSum(1)(2,2)(3,3,3)(4,4,4,4)(5,5,5,5,5))
+
+let looseCurriedSum = looseCurry(sum, 5)
+console.log(looseCurriedSum(1)(2,2,3)(3,3,4))
+
+module.exports = {
+    curry
+}
