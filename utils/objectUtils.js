6f19be95ac0b6aacbdb2e370dd3fdd327b11635e
luzaiping <142335@nd.com>
2017-07-19 18:01:58 +0800

    update：更新objectUtils.js

--- utils/objectUtils.js
+++ utils/objectUtils.js
@@ -5,6 +5,14 @@ function isEmpty(obj) {
     return typeUtils.isObject(obj) && Object.keys(obj).length === 0
 }
 
+/**
+ * 获取指定key的value，如果不存在则返回undefined，这个也可以用来判定object是否包含指定的key
+ * @param {*} key 
+ */
+function getKeyValue(key) {
+
+}
+
 module.exports = {
     isEmpty
 }
