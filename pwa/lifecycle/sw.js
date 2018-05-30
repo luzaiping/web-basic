63c46761f6a88754bdb7f56654578623e301528f
luzaiping <142335@nd.com>
2018-05-30 14:29:25 +0800

    update：完善 pwa lifecycle 例子，增加sw2.js，实现更新 service worker

--- pwa/lifecycle/sw.js
+++ pwa/lifecycle/sw.js
@@ -1,13 +1,19 @@
+/**
+ * install event 在 sw.js 被执行时立即被触发
+ * 另外每一个 worker 只会执行一次 install
+ * 但是如果修改 worker 脚本，会被当成是一个新的 worker，脚本会重新运行并再次执行install
+ */
 self.addEventListener('install', event => {
   console.log('V1 installing…');
 
   // cache a cat SVG
   event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/cat.jpg'))
+    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', () => {
+  // clients.claim()
   console.log('V1 now ready to handle fetches!');
 });
 
@@ -17,6 +23,6 @@ self.addEventListener('fetch', event => {
   // serve the cat SVG from the cache if the request is
   // same-origin and the path is '/dog.svg'
   if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/cat.jpg'));
+    event.respondWith(caches.match('/cat.svg'));
   }
 });
