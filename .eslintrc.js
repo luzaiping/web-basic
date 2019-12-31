2e7f6714b326ef31053139c1b340bb9235899750
luzaiping <lemhion1908@gmail.com>
2019-12-31 14:26:24 +0800

    dododo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  plugins: ["prettier"],
+  // eslint-config-prettier 会将跟 prettier 冲突的配置禁用掉，即 默认使用 prettier 的配置
+  extends: ['prettier'],
   globals: {
     /* window: true,
     document: true,
@@ -9,6 +10,9 @@ module.exports = {
     navigator: true,
     location: true */
   },
+  // Runs Prettier as an ESLint rule and reports differences as individual ESLint issues
+  // eslint-plugin-prettier 这个插件可以让 eslint 运行 prettier
+  plugins: ['prettier'],
   env: {
     browser: true,
     node: true
@@ -19,10 +23,10 @@ module.exports = {
     ecmaFeatures: {}
   },
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error', // prettier 作为 eslint rule 被校验
     indent: [0, 2],
-    quotes: [1, "single"],
-    semi: [0, "never"],
-    "no-unused-vars" : 1
+    quotes: [1, 'single'],
+    semi: [0, 'never'],
+    'no-unused-vars': 1
   }
-}
+};
