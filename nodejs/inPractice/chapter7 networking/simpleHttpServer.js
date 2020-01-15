e139cb5f01f8c42bfd5c336ef4494fb186765597
luzaiping <lemhion1908@gmail.com>
2020-01-15 15:20:21 +0800

    Update: nodejs -> networking -> simpleHttpServer

--- nodejs/inPractice/chapter7 networking/simpleHttpServer.js
+++ nodejs/inPractice/chapter7 networking/simpleHttpServer.js
@@ -4,6 +4,8 @@ const http = require('http');
 const message = 'Hello, world.';
 const port = 8000;
 
+// http 其实是 tcp socket，只是接收到请求，服务端返回response后会将 socket 关闭(也就是将connect断掉)
+// the underlying socket will be closed as soon as the response has been written.
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
@@ -16,6 +18,7 @@ server.listen(port, () => {
   console.log('Listening on port', port);
 });
 
+// http.request will create new connection
 const request = http.request({ port }, response => {
   const { headers, statusCode } = response;
   console.log('Http headers:', headers);
