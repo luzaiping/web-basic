c6d62510c094af20c6077fc5dc09a040b5a7397c
luzaiping <lemhion1908@gmail.com>
2020-01-15 17:24:37 +0800

    nodejs -> networking -> httpProxy

---
+++ nodejs/inPractice/chapter7 networking/httpProxy.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const url = require('url');
+
+http
+  .createServer((req, res) => {
+    console.log('start request: ', req.url);
+    const options = url.parse(req.url);
+    options.headers = req.headers;
+
+    const proxyRequest = http.request(options, proxyResponse => {
+      proxyResponse.on('data', chunk => {
+        console.log('proxyResponse length:', chunk.length);
+        res.write(chunk, 'binary');
+      });
+
+      proxyResponse.on('end', () => {
+        console.log('proxied request ended.');
+        res.end();
+      });
+
+      res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+    });
+
+    req.on('data', chunk => {
+      console.log('in request length:', chunk.length);
+      proxyRequest.write(chunk, 'binary');
+    });
+
+    req.on('end', () => {
+      console.log('original request ended.');
+      proxyRequest.end();
+    });
+  })
+  .listen(8080);
