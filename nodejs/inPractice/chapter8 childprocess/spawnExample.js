67684546bf4f4fb0f2ed1f0c3755a83395117851
luzaiping <lemhion1908@gmail.com>
2020-01-16 16:01:04 +0800

    Add: nodejs -> childprocess -> spawn
    Update: use fs.createReadStream instead of  cat

---
+++ nodejs/inPractice/chapter8 childprocess/spawnExample.js
@@ -0,0 +1,24 @@
+// U want to execute an external application and stream the output
+// Spawn is stream-based, it's great for handling large outputs or
+// working with data as it's read in.
+const childProcess = require('child_process');
+
+const fs = require('fs');
+
+// spawn method returns a ChildProcess object containing stdin
+// stdout,and stderr stream objects.
+/* const child = childProcess.spawn('echo', ['hello', 'world']);
+child.on('error', console.error); // Errors are emitted on error event.
+child.stdout.pipe(process.stdout); // Output from stdout and stderr can be read as it's available.
+child.stderr.pipe(process.stderr); */
+
+// const readStream = fs.createReadStream('./messy.txt');
+// spawn cat command is the same as above line.
+const cat = childProcess.spawn('cat', ['./messy.txt']);
+const sort = childProcess.spawn('sort');
+const uniq = childProcess.spawn('uniq');
+
+// readStream.pipe(sort.stdin);
+cat.stdout.pipe(sort.stdin);
+sort.stdout.pipe(uniq.stdin);
+uniq.stdout.pipe(process.stdout);
