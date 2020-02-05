ed34ffa24c21c02118b243ef15488703a3d248ba
luzaiping <lemhion1908@gmail.com>
2020-02-05 20:27:18 +0800

    feat: Set implementation

--- data-structure/learning/ch06 Set/Set.js
+++ data-structure/learning/ch06 Set/Set.js
@@ -1,3 +1,102 @@
 /**
  * Set data structure
  */
+
+class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  has(value) {
+    return Object.prototype.hasOwnProperty.call(this.items, value);
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.items[value] = value;
+      return true;
+    }
+    return false;
+  }
+
+  remove(value) {
+    if (this.items.has(value)) {
+      delete this.items[value];
+      return true;
+    }
+    return false;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  values() {
+    return Object.keys(this.items);
+  }
+
+  union(otherSet) {
+    const unionSet = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      unionSet.add(values[i]);
+    }
+
+    values = otherSet.values();
+
+    for (let i = 0; i < values.length; i++) {
+      unionSet.add(values[i]);
+    }
+
+    return unionSet;
+  }
+
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+
+    const values = this.values();
+
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i])) {
+        intersectionSet.add(values[i]);
+      }
+    }
+
+    return intersectionSet;
+  }
+
+  difference(otherSet) {
+    const differenceSet = new Set();
+
+    const values = this.values();
+
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i])) {
+        differenceSet.add(values[i]);
+      }
+    }
+
+    return differenceSet;
+  }
+
+  subset(otherSet) {
+    if (this.size() > otherSet.size()) return false;
+
+    const values = this.values();
+
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+module.exports = Set;
