d9bb64ad2c44d2ce72b8733964198208c7d7af97
luzaiping <lemhion1908@gmail.com>
2020-02-06 15:44:30 +0800

    feat: add a better hashes function
    add djbToHashCode as better hashes function to reduce collisions

--- data-structure/learning/ch07 Hash&Dictionary/hashCodeUtils.js
+++ data-structure/learning/ch07 Hash&Dictionary/hashCodeUtils.js
@@ -7,6 +7,16 @@ exports.loseloseHashCode = function(key) {
   return hash % 37;
 };
 
+exports.djbToHashCode = function(key) {
+  let hash = 5381;
+
+  for (let i = 0; i < key.length; i++) {
+    hash = hash * 33 + key.charCodeAt(i);
+  }
+
+  return hash % 33;
+};
+
 exports.Node = function(key, value) {
   if (typeof new.target !== 'undefined') {
     this.key = key;
