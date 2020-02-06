5f77bd00a900ab9ab1a39680a978564e6f418924
luzaiping <lemhion1908@gmail.com>
2020-02-06 20:41:08 +0800

    feat: 实现一个二叉搜索树
    实现一个二叉搜索树，包含 insert, search, remove, min,max 和 3个遍历节点的方法
    
    main.js 是用于测试 二叉搜索树 的实现

---
+++ data-structure/learning/ch08 Tree/BinarySearchTree.js
@@ -0,0 +1,194 @@
+/* eslint-disable no-lonely-if */
+/* eslint-disable no-param-reassign */
+/**
+ * 实现一个二叉搜索树(BST)
+ */
+
+function Node(key) {
+  if (typeof new.target !== 'undefined') {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function insertNode(node, newNode) {
+  if (newNode.key < node.key) {
+    if (node.left === null) {
+      node.left = newNode;
+    } else {
+      insertNode(node.left, newNode);
+    }
+  } else {
+    if (node.right === null) {
+      node.right = newNode;
+    } else {
+      insertNode(node.right, newNode);
+    }
+  }
+}
+
+/**
+ * 有序遍历，从一个二叉搜索树(BST) 的最小值节点到最大值节点
+ * @param {Node} node 树节点
+ * @param {Function} callback 处理节点的函数, 这个就是 visitor pattern
+ */
+function inOrderTraverseNode(node, callback) {
+  if (node !== null) {
+    inOrderTraverseNode(node.left, callback);
+    callback(node);
+    inOrderTraverseNode(node.right, callback);
+  }
+}
+
+/**
+ * 先访问父节点，再访问子节点(先左后右)
+ * @param {Node} node 树节点
+ * @param {Function} callback 处理节点的函数, 这个就是 visitor pattern
+ */
+function preOrderTraverseNode(node, callback) {
+  if (node !== null) {
+    callback(node);
+    preOrderTraverseNode(node.left, callback);
+    preOrderTraverseNode(node.right, callback);
+  }
+}
+
+/**
+ * 先访问子节点(先左后右)，再访问父节点
+ * @param {Node} node 树节点
+ * @param {Function} callback 处理节点的函数, 这个就是 visitor pattern
+ */
+function postOrderTraverseNode(node, callback) {
+  if (node !== null) {
+    postOrderTraverseNode(node.left, callback);
+    postOrderTraverseNode(node.right, callback);
+    callback(node);
+  }
+}
+
+function findMinNode(node) {
+  if (node) {
+    while (node && node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+  return null;
+}
+
+/**
+ * 搜索最小树节点
+ * @param {Node} node
+ */
+function minNode(node) {
+  const target = findMinNode(node);
+  return target && target.key;
+}
+
+function maxNode(node) {
+  if (node) {
+    while (node && node.right) {
+      node = node.left;
+    }
+    return node.key;
+  }
+  return null;
+}
+
+function searchNode(node, key) {
+  if (node) {
+    if (node.key < key) {
+      return searchNode(node.right, key);
+    }
+    if (node.key > key) {
+      return searchNode(node.left, key);
+    }
+    return true;
+  }
+
+  return false;
+}
+
+function removeNode(node, key) {
+  if (node === null) {
+    return null;
+  }
+
+  if (key < node.key) {
+    node.left = removeNode(node.left, key);
+    return node;
+  }
+
+  if (key > node.key) {
+    node.right = removeNode(node.right, key);
+    return node;
+  }
+
+  // by returning null U can assign null to its parent node
+  if (node.left === null && node.right === null) {
+    node = null;
+    return node;
+  }
+
+  if (node.left === null) {
+    node = node.right;
+    return node;
+  }
+
+  if (node.right === null) {
+    node = node.left;
+    return node;
+  }
+
+  const aux = findMinNode(node.right);
+  node.key = aux.key;
+  node.right = removeNode(node.right, aux.key);
+  return node;
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    const node = new Node(key);
+
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      insertNode(this.root, node);
+    }
+  }
+
+  inOrderTraverse(callback) {
+    inOrderTraverseNode(this.root, callback);
+  }
+
+  preOrderTraverse(callback) {
+    preOrderTraverseNode(this.root, callback);
+  }
+
+  postOrderTraverse(callback) {
+    postOrderTraverseNode(this.root, callback);
+  }
+
+  min() {
+    return minNode(this.root);
+  }
+
+  max() {
+    return maxNode(this.root);
+  }
+
+  search(key) {
+    return searchNode(this.root, key);
+  }
+
+  remove(key) {
+    this.root = removeNode(this.root, key);
+  }
+}
+
+module.exports = BinarySearchTree;
---
+++ data-structure/learning/ch08 Tree/main.js
@@ -0,0 +1,31 @@
+const BinarySearchTree = require('./BinarySearchTree');
+
+const tree = new BinarySearchTree();
+
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
+tree.insert(6);
+
+function printNode(node) {
+  console.log(node.key);
+}
+
+tree.inOrderTraverse(printNode);
+console.log('===================');
+tree.preOrderTraverse(printNode);
+console.log('===================');
+tree.postOrderTraverse(printNode);
+console.log('===================');
+console.log(tree.search(6));
