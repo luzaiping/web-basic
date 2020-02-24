a266f7dc7ae6bf5a0c1fc969c596118b68058a01
luzaiping <lemhion1908@gmail.com>
2020-02-24 17:54:01 +0800

    feat: 引入 jest 测试框架
    - 修复 Cannot read property 'bindings' of null，这个需要更新 babel
    - 增加一个基本的测试实例
    - 引入 yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
+  // eslint-plugin-prettier is a plugin that adds a rule that formats content using Prettier
+  plugins: ['prettier'],
   // eslint-config-prettier 会将跟 prettier 冲突的配置禁用掉，即 默认使用 prettier 的配置
   extends: ['airbnb-base', 'prettier'],
+  // the additional global variables your script accesses during execution.
   globals: {
     /* window: true,
     document: true,
@@ -12,17 +15,27 @@ module.exports = {
   },
   // Runs Prettier as an ESLint rule and reports differences as individual ESLint issues
   // eslint-plugin-prettier 这个插件可以让 eslint 运行 prettier
-  plugins: ['prettier'],
   env: {
+    // Each environment brings with it a certain set of predefined global variables.
     browser: true,
+    es6: true, // supporting new ES6 global such as Set, Map. This will enable ES6 syntax automatically,
     node: true,
+    jest: true,
     mocha: true // support mocha
   },
+  // Specify the Javascript language option you want to support.
+  // Setting parser options helps ESLint determine what is a parsing error.
+  // All language options are false by default.
   parserOptions: {
-    ecmaVersion: 2018, // 这个可以解决 spread 和 rest operator 问题
-    sourceType: 'module',
-    ecmaFeatures: {}
+    ecmaVersion: 2018, // 这个可以解决 spread 和 rest operator 以及 async await. 默认值是 5
+    sourceType: 'module', // 默认值是 'script', 'module' 表示使用 ES Module
+    ecmaFeatures: {
+      // globalReturn: true, 允许在全局作用域里使用 return 语句，这个一般不配，很少使用
+      // impliedStrict: true, 允许全局的 strict mode 如果 ecmaversion >= 5
+      jsx: true // supporting jsx syntax. 注意这个不表示支持 React. React 对jsx应用了 eslint 无法识别的特别的语义。如果要支持 React，要用 eslint-react-plugin
+    }
   },
+  // while rules are enabled and at what error level.
   rules: {
     'func-names': 0,
     'no-console': 0,
--- unit-test/chai/chai.spec.js
+++ unit-test/chai/chai.spec.js
@@ -1,77 +1,82 @@
-let chai = require('chai')
+let chai = require('chai');
 
 let assertCase = () => {
-    let assert = chai.assert,
-        foo = 'bar',
-        beverages = {
-            teas: ['chai', 'mocha', 'oolong']
-        }
+  let assert = chai.assert,
+    foo = 'bar',
+    beverages = {
+      teas: ['chai', 'mocha', 'oolong']
+    };
 
-    describe('chai assert case', function () {
-        it('should be a string type', function() {
-            assert.typeOf(foo, 'string')
-        })
-        it('should be a string type, otherwise throws error message', function() {
-            assert.typeOf(foo, 'string', 'foo must be a string')
-        })
-        it('foo should be 3 length', function() {
-            assert.lengthOf(foo, 3, 'foo must be a string with length 3')
-        })
-        it('foo should be equal to "bar"', function() {
-            assert.equal(foo, 'bar', '')
-        })
-    })
-}
+  describe('chai assert case', function() {
+    it('should be a string type', function() {
+      assert.typeOf(foo, 'string');
+    });
+    it('should be a string type, otherwise throws error message', function() {
+      assert.typeOf(foo, 'string', 'foo must be a string');
+    });
+    it('foo should be 3 length', function() {
+      assert.lengthOf(foo, 3, 'foo must be a string with length 3');
+    });
+    it('foo should be equal to "bar"', function() {
+      assert.equal(foo, 'bar', '');
+    });
+  });
+};
 
 let expectCase = () => {
-    let expect = chai.expect,
-        foo = 'bar',
-        beverages = {
-            'tea': ['chai', 'mocha', 'oolong']
-        }
-    let goodFn = () => {}
-    let deepObj = { foo: {bar: { baz: 'quux' }}}
-    describe('expect chainable style', function() {
-        it('foo String', function() {
-            expect(foo).to.be.a('string').to.equal('bar').to.have.lengthOf(3)
-        })
-        it('beverages Array', function() {
-            expect(beverages).to.have.property('tea').with.lengthOf(3)
-        })
-        it('good function', function() {
-            expect(goodFn).to.not.throw(Error)
-        })
-        it('deep object', function() {
-            expect(deepObj).to.have.deep.property('foo.bar.baz', 'quux')
-            expect(deepObj).to.have.any.keys('foo')
-        })
-    })
-}
+  let expect = chai.expect,
+    foo = 'bar',
+    beverages = {
+      tea: ['chai', 'mocha', 'oolong']
+    };
+  let goodFn = () => {};
+  let deepObj = { foo: { bar: { baz: 'quux' } } };
+  describe('expect chainable style', function() {
+    it('foo String', function() {
+      expect(foo)
+        .to.be.a('string')
+        .to.equal('bar')
+        .to.have.lengthOf(3);
+    });
+    it('beverages Array', function() {
+      expect(beverages)
+        .to.have.property('tea')
+        .with.lengthOf(3);
+    });
+    it('good function', function() {
+      expect(goodFn).to.not.throw(Error);
+    });
+    it('deep object', function() {
+      expect(deepObj).to.have.deep.property('foo.bar.baz', 'quux');
+      expect(deepObj).to.have.any.keys('foo');
+    });
+  });
+};
 
 let shouldCase = () => {
-    chai.should()
+  chai.should();
 
-    describe('chai should chainable style', function() {
-        it('array should return -1 when the value is not present', function() {
-            [1, 2, 3].indexOf(5).should.equal(-1);
-            [1, 2, 3].indexOf(0).should.equal(-1)
-        })
-    })
-}
+  describe('chai should chainable style', function() {
+    it('array should return -1 when the value is not present', function() {
+      [1, 2, 3].indexOf(5).should.equal(-1);
+      [1, 2, 3].indexOf(0).should.equal(-1);
+    });
+  });
+};
 
 let chaiAsPromisedCase = () => {
-    let chaiAsPromised = require('chai-as-promised')
-    let expect = chai.expect
-    chai.use(chaiAsPromised)
-    describe('chai-as-promised', function() {
-        it('resolve promise', function() {
-            return expect(Promise.resolve( 2 + 2 )).to.eventually.equal(4)
-        })
-    })
-}
+  let chaiAsPromised = require('chai-as-promised');
+  let expect = chai.expect;
+  chai.use(chaiAsPromised);
+  describe('chai-as-promised', function() {
+    it('resolve promise', function() {
+      return expect(Promise.resolve(2 + 2)).to.eventually.equal(4);
+    });
+  });
+};
 
-assertCase()
+assertCase();
 
-expectCase()
+expectCase();
 
-shouldCase()
+shouldCase();
