10b4e882a1c803908665e9ce62261fa30ace185f
luzaiping <lemhion1908@gmail.com>
2020-06-22 17:23:46 +0800

    feat: 。。。。

--- checklist/react/fiber.js
+++ checklist/react/fiber.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-param-reassign */
+const TEXT_TYPE = 'TEXT_ELEMENT';
+let nextUnitOfWork = null;
 
-const isEvent = key => key.startsWith('on');
-const isProperty = key => key !== 'children' && !isEvent(key);
-const isNew = (prev, next) => key => prev[key] !== next[key];
-const isGone = (prev, next) => key => !(key in next);
+const isValidProp = key => key !== 'children';
 
 function createTextElement(text) {
   return {
-    type: 'TEXT_ELEMENT',
+    type: TEXT_TYPE,
     props: {
       nodeValue: text,
       children: []
@@ -27,182 +26,71 @@ function createElement(type, props, ...children) {
   };
 }
 
-function updateDom(dom, prevProps, nextProps) {
-  // remove old or changed event listeners
-  Object.keys(prevProps)
-    .filter(isEvent)
-    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2);
-      dom.removeEventListener(eventType, prevProps[name]);
-    });
-
-  // remove old properties
-  Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = '';
-    });
-
-  // Set new or changed properties
-  Object.keys(nextProps)
-    .filter(isProperty)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = nextProps[name];
-    });
-
-  // Add event listeners
-  Object.keys(nextProps)
-    .filter(isEvent)
-    .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2);
-      dom.addEventListener(eventType, nextProps[name]);
-    });
-}
-
 /**
- * 创建 fiber 对应的 dom 元素
+ * 实现 fiber -> dom
  * @param {Object} fiber
  */
 function createDom(fiber) {
   const dom =
-    fiber.type === 'TEXT_ELEMENT'
+    fiber.type === TEXT_TYPE
       ? document.createTextNode('')
       : document.createElement(fiber.type);
 
-  // 将非 children 的 props 添加到 dom 节点上
-  // 作为 dom 节点的 attribute
-  updateDom(dom, {}, fiber.props);
+  Object.keys(fiber.props)
+    .filter(isValidProp)
+    .forEach(key => {
+      dom[key] = fiber.props[key];
+    });
 
   return dom;
 }
 
-// 这个 render 是 react 16之前的实现方式：
-// 不管 element tree 有多大，始终要渲染完才会让出资源
-/* function render(element, container) {
-  // 根据元素类型创建对应的 dom 元素
-  const dom = createDom(element);
-
-  // 递归处理 children
-  element.props.children.forEach(child => {
-    render(child, dom);
-  });
-
-  container.appendChild(dom);
-} */
-
-let nextUnitOfWork = null;
-let WIPRoot = null; // WIP: work-in-progress
-let currentRoot = null;
-let deletions = null;
-
-function commitWork(fiber) {
-  if (!fiber) return;
-
-  const domParent = fiber.parent.dom;
-  if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
-    domParent.appendChild(fiber.dom);
-  } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom);
-  } else if (fiber.effectTag === 'UPDATE' && fiber.dom !== null) {
-    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
-  }
-  commitWork(fiber.child);
-  commitWork(fiber.sibling);
-}
-
-function commitRoot() {
-  // TODO add nodes to dom
-  deletions.forEach(commitWork);
-  commitWork(WIPRoot.child);
-  currentRoot = WIPRoot;
-  WIPRoot = null;
-}
-
-// set nextUnitOfWork to the root of the fiber tree
+// 设置初始 nextUnitOfWork
 function render(element, container) {
-  WIPRoot = {
+  nextUnitOfWork = {
     dom: container,
     props: {
       children: [element]
-    },
-    alternate: currentRoot
+    }
   };
-  nextUnitOfWork = WIPRoot;
-  deletions = [];
 }
 
-function reconcileChildren(WIPFiber, elements) {
+function performUnitOfWork(fiber) {
+  // first create a new node and append it to the DOM.
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
   let index = 0;
-  let oldFiber = WIPFiber.alternate && WIPFiber.alternate.child;
   let prevSibling = null;
 
-  while (index < elements.length || oldFiber !== null) {
+  while (index < elements.length) {
     const element = elements[index];
-    let newFiber = null;
 
-    const sameType = oldFiber && element && element.type === oldFiber.type;
-
-    if (sameType) {
-      // update the node
-      newFiber = {
-        type: oldFiber.type,
-        props: element.props,
-        dom: oldFiber.dom,
-        parent: WIPFiber,
-        alternate: oldFiber,
-        effectTag: 'UPDATE'
-      };
-    }
-
-    if (element && !sameType) {
-      // add this node
-      newFiber = {
-        type: element.type,
-        props: element.props,
-        dom: null,
-        parent: WIPFiber,
-        alternate: null,
-        effectTag: 'PLACEMENT'
-      };
-    }
-
-    if (oldFiber && !sameType) {
-      // delete the oldFiber's node
-      oldFiber.effectTag = 'DELETION';
-      deletions.push(oldFiber);
-    }
-
-    if (oldFiber) {
-      oldFiber = oldFiber.sibling;
-    }
+    // for each child, we create a new fiber
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    };
 
     if (index === 0) {
-      WIPFiber.child = newFiber;
-    } else if (element) {
+      fiber.child = newFiber;
+    } else {
       prevSibling.sibling = newFiber;
     }
 
     prevSibling = newFiber;
     index++;
   }
-}
 
-function performUnitOfWork(fiber) {
-  // add a dom node
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
-
-  if (fiber.child) {
-    return fiber.child;
-  }
+  if (fiber.child) return fiber.child;
 
   let nextFiber = fiber;
   while (nextFiber) {
@@ -215,66 +103,34 @@ function performUnitOfWork(fiber) {
   return null;
 }
 
-function workLoop(deadline) {
+function workLoop(deadLine) {
   let shouldYield = false;
+
   while (nextUnitOfWork && !shouldYield) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
-    shouldYield = deadline.timeRemaining() < 1;
-  }
-
-  if (!nextUnitOfWork && WIPRoot) {
-    commitRoot();
+    shouldYield = deadLine.timeRemaining() < 1;
   }
 
   requestIdleCallback(workLoop);
 }
 
-// browser 会在恰当的时机调用这个函数
-requestIdleCallback(workLoop);
-
-const Didact = {
-  createElement
-};
-
-const DidactDOM = {
+const SReactDOM = {
   render
 };
 
-const container = document.getElementById('root');
-
-const rerender = value => {
-  const element = Didact.createElement(
-    'div',
-    null,
-    Didact.createElement(
-      'input',
-      {
-        // eslint-disable-next-line no-use-before-define
-        onInput: updateValue,
-        value
-      },
-      null
-    ),
-    Didact.createElement('h2', null, `Hello ${value}`)
-  );
-
-  DidactDOM.render(element, container);
+const SReact = {
+  createElement
 };
 
-function updateValue(e) {
-  rerender(e.target.value);
-}
+const element = SReact.createElement(
+  'div',
+  { id: 'foo' },
+  SReact.createElement('a', null, 'bar'),
+  SReact.createElement('b')
+);
 
-rerender('world');
+const container = document.getElementById('root');
 
-// 下面注释是告知 babel 在转换下面的 jsx
-// 时使用我们定义的 Didact.createElement 函数
-/** @jsx Didact.createElement */
-// const element = (
-//   <div id="foo">
-//     <a>bar</a>
-//     <b />
-//   </div>
-// );
+SReactDOM.render(element, container);
 
-// console.log('render', JSON.stringify(element));
+requestIdleCallback(workLoop);
