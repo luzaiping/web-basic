91a8326bb61562673f3a784a57d5ddee612d3833
luzaiping <lemhion1908@gmail.com>
2020-06-23 14:09:25 +0800

    feat: 增强 hooks 实现
    通过定义 hooks 数组 和 currentHook 以支持多个 hooks 的场景

--- checklist/react/hooks.js
+++ checklist/react/hooks.js
@@ -14,48 +14,60 @@ setValue(2);
 console.log('更新值', value); */
 
 const MyReact = (function() {
-  let value;
-  let deps;
+  const hooks = []; // 存放所有的 hook
+  let currentHook = 0; // 当前 hook, 每调用一次 useXXX，就 currentHook 索引就加 1
   return {
     render(Component) {
       const Comp = Component();
       Comp.render();
+      currentHook = 0; // 复位，为下一次 rerender 做准备
       return Comp;
     },
     useState(initialValue) {
-      value = value || initialValue;
+      hooks[currentHook] = hooks[currentHook] || initialValue; // type: any
+
+      // 给 setState 闭包准备的变量，因为 currentHook 在每次 render 都会重置为 0
+      // 这边需要一个 setStateHookIndex 来保持值
+      const setStateHookIndex = currentHook;
 
       function setState(newValue) {
-        value = newValue;
+        hooks[setStateHookIndex] = newValue;
       }
 
-      return [value, setState];
+      return [hooks[currentHook++], setState];
     },
     useEffect(callback, depArray) {
       const hasNoDeps = !depArray; // 没有依赖，每次 render 都会运行 callback
+      const deps = hooks[currentHook]; // type: array | undefined
+
       // 有一个 dep 发生变化，就需要调用 callback
       const hasDepsChanged = !deps || depArray.some((el, i) => el !== deps[i]); // deps 发生变化
       if (hasNoDeps || hasDepsChanged) {
         callback();
-        deps = depArray;
+        hooks[currentHook] = depArray;
       }
+      currentHook++;
     }
   };
 })();
 
 function Counter() {
   const [count, setCount] = MyReact.useState(1);
+  const [text, setText] = MyReact.useState('hello');
 
   MyReact.useEffect(() => {
-    console.log('effect:', count);
-  }, [count]);
+    console.log('effect:', count, text);
+  }, [count, text]);
 
   return {
     click: () => {
       setCount(count + 1);
     },
+    updateText: nextText => {
+      setText(nextText);
+    },
     render: () => {
-      console.log('render：', { count });
+      console.log('render：', { count, text });
     }
   };
 }
@@ -70,4 +82,6 @@ function Counter() {
 // 下面这个使用方式，调用了 MyReact.render 后，通过 App.click 获取到的 count 就是最新值
 let App = MyReact.render(Counter);
 App.click();
+App = MyReact.render(Counter);
+App.updateText('world');
 App = MyReact.render(Counter);
