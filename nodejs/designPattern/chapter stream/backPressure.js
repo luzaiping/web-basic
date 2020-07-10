2cb94960f86d6da89e9581a55511488685e3dd39
luzaiping <lemhion1908@gmail.com>
2020-07-10 10:25:16 +0800

    feat: 增加 backPressure 注释内容，解释 drain 的触发机制

--- nodejs/designPattern/chapter stream/backPressure.js
+++ nodejs/designPattern/chapter stream/backPressure.js
@@ -8,20 +8,26 @@ http
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     function generateMore() {
+      console.log('========== generateMore ==========');
+      // 95% 的可能会返回 true
       while (chance.bool({ likelihood: 95 })) {
         const length = 16 * 1024 - 1;
         const shouldContinue = res.write(`${chance.string({ length })}`);
         if (!shouldContinue) {
           console.log('BackPressure');
-          res.once('drain', generateMore);
-          break;
+
+          // 使用 return 会中断 generateMore 函数的执行；等 drain 事件被触发后，重新执行 generateMore
+          return res.once('drain', generateMore);
+
+          // 使用 break 会跳出 while 循环，然后往下执行 res.end，最终结束整个输出流，所以不会触发 drain 事件
+          // res.once('drain', generateMore);
+          // break;
         }
       }
-      res.end('\n The end ... \n');
-      res.on('finish', () => {
-        console.log('All data was sent.');
-      });
+      res.end('\n The end ... \n', console.log('All data was sent.'));
+      return null;
     }
+
     generateMore();
   })
   .listen(8080, () => {
