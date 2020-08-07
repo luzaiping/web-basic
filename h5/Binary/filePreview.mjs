3fa5f63fc70e0880a0a7bda7445ccd5101d36498
zaiping.lu <zaiping.lu@luckincoffee.com>
2020-08-07 17:07:54 +0800

    feat: 更新图片预览功能
    分别使用 ObjectURL 和 DataURL 实现功能

--- h5/Binary/filePreview.mjs
+++ h5/Binary/filePreview.mjs
@@ -1,18 +1,33 @@
 const fileInput = document.querySelector('.previewImage');
 const previewContainer = document.querySelector('.previewContainer');
+const isObjectURl = true;
+let blobUrl;
 
 fileInput.addEventListener(
   'change',
   e => {
     const file = e.target.files[0]; // 被选择的文件，是一个 File 对象
 
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file); // 将 Blob 转换为 DataURL
+    function previewUsingObjectURL() {
+      blobUrl = URL.createObjectURL(file); // 将 blob 转换为 Object URL，格式 blob:http://127.0.0.1:5000/073c6b98-d05f-4b06-91d6-d9aba8ed5733
+      previewContainer.src = blobUrl;
 
-    // load event is fired when a file has been read successfully.
-    fileReader.onload = () => {
-      previewContainer.src = fileReader.result;
-    };
+      previewContainer.onload = () => {
+        blobUrl && URL.revokeObjectURL(blobUrl);
+      };
+    }
+
+    function previewUsingDataURL() {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file); // 将 Blob 转换为 DataURL
+
+      // load event is fired when a file has been read successfully.
+      fileReader.onload = () => {
+        previewContainer.src = fileReader.result;
+      };
+    }
+
+    isObjectURl ? previewUsingObjectURL() : previewUsingDataURL();
   },
   false
 );
